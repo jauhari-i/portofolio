553b5ccd72b6c5c6f4261caea1ba0f999a742f72
Irfan Jauhari <kangrykong123@gmail.com>
2020-07-31 22:39:41 +0700

    Updates

---
+++ precache-manifest.a8f5ab5985dbef5ecd34506a0a31b20e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5b00c1fbd547e6f9c918f50a15ddc8",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "cdf332bd0cf5c39c769f",
+    "url": "/portofolio/static/css/main.cd5868bd.chunk.css"
+  },
+  {
+    "revision": "2bf28f9b900908408eec",
+    "url": "/portofolio/static/js/2.f5f86112.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/portofolio/static/js/2.f5f86112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf332bd0cf5c39c769f",
+    "url": "/portofolio/static/js/main.c6429a8b.chunk.js"
+  },
+  {
+    "revision": "a3a6c8a1f81f721d2b09",
+    "url": "/portofolio/static/js/runtime-main.cef59536.js"
+  },
+  {
+    "revision": "d6a5cd392dd974b7c05a8dbc53896ed2",
+    "url": "/portofolio/static/media/black.d6a5cd39.jpg"
+  }
+]);
